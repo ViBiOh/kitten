8f9c58acd5d637b93225240c28a5abe703d86bfa
Vincent Boutour <bob@vibioh.fr>
2022-03-30 13:47:31 +0200

    feat: Adding cache in yolo mode
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/kitten/kitten.go
+++ pkg/kitten/kitten.go
@@ -1,23 +1,39 @@
 package kitten
 
 import (
+	"bytes"
+	"context"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"image"
 	"image/png"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/ViBiOh/httputils/v4/pkg/httperror"
+	"github.com/ViBiOh/httputils/v4/pkg/logger"
+	"github.com/ViBiOh/httputils/v4/pkg/redis"
+	"github.com/ViBiOh/httputils/v4/pkg/sha"
 	"github.com/ViBiOh/kitten/pkg/meme"
 	"github.com/ViBiOh/kitten/pkg/unsplash"
 )
 
-var cacheDuration = fmt.Sprintf("public, max-age=%.0f", time.Duration(time.Hour*24).Seconds())
+var (
+	bufferPool = sync.Pool{
+		New: func() any {
+			return bytes.NewBuffer(make([]byte, 32*1024))
+		},
+	}
+
+	cacheDuration        = time.Hour * 24
+	cacheControlDuration = fmt.Sprintf("public, max-age=%.0f", cacheDuration.Seconds())
+)
 
 // Handler for Hello request. Should be use with net/http
-func Handler(memeApp meme.App) http.Handler {
+func Handler(memeApp meme.App, redisApp redis.App) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
@@ -32,12 +48,25 @@ func Handler(memeApp meme.App) http.Handler {
 			return
 		}
 
+		id := strings.TrimSpace(query.Get("id"))
+		from := strings.TrimSpace(query.Get("from"))
+
+		if content, err := redisApp.Load(r.Context(), getCacheKey(id, from, caption)); err == nil && len(content) > 0 {
+			if payload, err := base64.RawStdEncoding.DecodeString(content); err != nil {
+				logger.Error("unable to decode image from cache: %s", err)
+			} else {
+				w.Header().Add("Cache-Control", cacheControlDuration)
+				w.Header().Set("Content-Type", "image/png")
+				if _, err = w.Write(payload); err != nil {
+					logger.Error("unable to write image from cache: %s", err)
+				}
+			}
+		}
+
 		var image image.Image
 		var details unsplash.Image
 		var err error
 
-		id := strings.TrimSpace(query.Get("id"))
-		from := strings.TrimSpace(query.Get("from"))
 		search := strings.TrimSpace(query.Get("search"))
 
 		if len(from) != 0 {
@@ -54,17 +83,34 @@ func Handler(memeApp meme.App) http.Handler {
 		}
 
 		if !details.IsZero() {
+			id = details.ID
+
 			w.Header().Set("X-Image-ID", details.ID)
 			w.Header().Set("X-Image-Author", details.Author)
 			w.Header().Set("X-Image-Author-URL", details.AuthorURL)
 		}
 
-		w.Header().Add("Cache-Control", cacheDuration)
+		w.Header().Add("Cache-Control", cacheControlDuration)
 		w.Header().Set("Content-Type", "image/png")
 		w.WriteHeader(http.StatusOK)
 		if err = png.Encode(w, image); err != nil {
 			httperror.InternalServerError(w, err)
 			return
 		}
+
+		go func() {
+			buffer := bufferPool.Get().(*bytes.Buffer)
+			defer bufferPool.Put(buffer)
+
+			if err := png.Encode(buffer, image); err != nil {
+				logger.Error("unable to encode image for cache: %s", err)
+			} else if err = redisApp.Store(context.Background(), getCacheKey(id, from, caption), base64.RawStdEncoding.EncodeToString(buffer.Bytes()), cacheDuration); err != nil {
+				logger.Error("unable to write image to cache: %s", err)
+			}
+		}()
 	})
+}
+
+func getCacheKey(id, from, caption string) string {
+	return "kitten:" + sha.New(fmt.Sprintf("%s:%s:%s", id, from, caption))
 }
