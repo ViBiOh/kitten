6dd1a40dc45d55e286afa6361426fae521856ec7
Vincent Boutour <bob@vibioh.fr>
2026-02-12 21:40:01 +0100

    refactor: Using astype
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/unsplash/unplash.go
+++ pkg/unsplash/unplash.go
@@ -120,8 +120,7 @@ func (s Service) Search(ctx context.Context, query string) (Image, error) {
 			return Image{}, ErrRateLimitExceeded
 		}
 
-		var httpError request.Error
-		if errors.As(err, &httpError) && httpError.StatusCode == http.StatusNotFound {
+		if httpError, ok := errors.AsType[request.Error](err); ok && httpError.StatusCode == http.StatusNotFound {
 			return Image{}, httperror.FromResponse(resp, fmt.Errorf("nothing was found for the query `%s`", query))
 		}
 
